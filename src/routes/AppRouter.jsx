38c6274704619746324d3c10d450be179d188238
gitcat456 <okothdenzel65@gmail.com>
2025-09-11 14:23:44 +0300

    Creaates a route folder

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from '../components/layout';
+import Home from '../pages/Home';
+
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
