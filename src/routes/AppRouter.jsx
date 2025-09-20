d78d60c8d7f48b28b7a92675bec10cdc7cbb397b
gitcat456 <okothdenzel65@gmail.com>
2025-09-20 18:07:32 +0300

    Adds route for events section

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Layout from '../components/layout';
 import Home from '../pages/Home';
+import Events from '../pages/Events';
 
 
 const AppRoutes = () => {
@@ -8,6 +9,7 @@ const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="events" element={<Events />} />
         <Route path="*" element={<h1>404 - Page Not Found</h1>} />
       </Route>
     </Routes>
