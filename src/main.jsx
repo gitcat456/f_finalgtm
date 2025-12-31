7b0ba3d8583edbbf1807a8c6f75d4bb4f861785f
gitcat456 <okothdenzel65@gmail.com>
2025-12-31 23:05:58 +0300

    minor refactors

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LoadingSpinner from './components/LoadingSpinner.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Suspense fallback={<LoadingSpinner />}>
+      <App />
+    </Suspense>
   </StrictMode>,
 )
