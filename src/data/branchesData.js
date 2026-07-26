ac4193ccc370325dc5857855ebc5743835fcb918
gitcat456 <okothdenzel65@gmail.com>
2026-07-26 16:25:08 +0300

    Update leadership contanct

--- src/data/branchesData.js
+++ src/data/branchesData.js
@@ -1,9 +1,8 @@
 import oriang from "../assets/oriang_pastor.JPG";
 import kanyipir from "../assets/kanyipir_pastor.jpeg";
-import rawinji from "../assets/rawinji_pastor.jpeg";
 import matharedp from "../assets/matharedp.jpg";
 import br1 from "../assets/br1.webp";
-import br2 from "../assets/br2one.webp";
+import br2one from "../assets/br2one.webp";
 import br3 from "../assets/br3.webp";
 import br4 from "../assets/br4.avif";
 import br5 from "../assets/br5.avif";
@@ -13,125 +12,138 @@ import br8 from "../assets/br8.jpg";
 import br9 from "../assets/br9.jpg";
 import br10 from "../assets/br10.avif";
 
-
 export const branches = [
   {
     id: 1,
     name: "Mathare Church",
     location: "Mathare 4A, Nairobi",
-    contact1: "+254 714 707462",
-    pastor1: "Pastor Dancun Ongoro",
-    pastor2: "Pastor Susan Omondi",
-    contact2: "+254 726 327537",
     services: "Saturday: 9am to 1pm",
-    img: matharedp
+    img: matharedp,
+    pastors: [
+      { name: "Pastor Dancun Ongoro", contact: "+254 714 707462" },
+      { name: "Pastor Susan Omondi", contact: "+254 726 327537" }
+    ]
   },
   {
     id: 2,
     name: "Kiambio Branch",
     location: "Kiambio, Nairobi",
-    pastor: "Pastor Rosemary",
-    contact: "+254 700 000000",
     services: "Saturday: 9am to 1pm",
-    img: br1
-    },
+    img: br1,
+    pastors: [
+      { name: "Pastor Rosemary", contact: "+254 713 842213" }
+    ]
+  },
   {
     id: 3,
     name: "LuckySummer Branch",
-    location: "LuckSummer, Nairobi",
-    pastor: "Pastor Lilian",
-    contact: "+254 700 000000",
-    services: "Saturday 9am to 1pm ",
-    img: br2
+    location: "LuckySummer, Nairobi",
+    services: "Saturday 9am to 1pm",
+    img: br2one,
+    pastors: [
+      { name: "Pastor Lilian", contact: "+254 795 981513" }
+    ]
   },
   {
     id: 4,
     name: "Rongai Branch",
     location: "Rongai, Nairobi",
-    pastor: "Pastor Lilian",
-    contact: "+254 700 000000",
-    services: "Saturday 9am to 1pm ",
-    img: br3
+    services: "Saturday 9am to 1pm",
+    img: br3,
+    pastors: [
+      { name: "Pastor Lilian", contact: "+254 7" }
+    ]
   },
   {
     id: 5,
-    name: "Obama Branch",
+    name: "Kayole Junction Branch",
     location: "Obama Estate",
-    pastor: "Pastor Fred Dede",
-    contact: "+254 700 000000",
-    services: "Saturday 9am to 1pm ",
-    img: br4
+    services: "Saturday 9am to 1pm",
+    img: br4,
+    pastors: [
+      { name: "Pastor Susan Omondi", contact: "+254 726 327537" }
+    ]
   },
   {
     id: 6,
     name: "Ruaraka Branch",
     location: "Mathare North Area2, Nairobi",
-    pastor: "Pastor Oromo Samson",
-    contact: "+254 700 000000",
-    services: "Saturday 9am to 1pm ",
-    img: br5
+    services: "Saturday 9am to 1pm",
+    img: br5,
+    pastors: [
+      { name: "Pastor Oromo Samson", contact: "+254 721 104678" }
+    ]
   },
   {
     id: 7,
     name: "Kisumu Branch",
     location: "Nyamasaria, Kisumu",
-    pastor: "Pastor James Ochieng",
-    contact: "+254 725 106547",
-    services: "Saturday 9am to 1pm ",
-    img: br6
+    services: "Saturday 9am to 1pm",
+    img: br6,
+    pastors: [
+      { name: "Pastor James Ochieng", contact: "+254 725 106547" }
+    ]
   },
   {
     id: 8,
     name: "Oriang Church",
     location: "Oriang, Kendubay",
-    pastor: "Pastor John Ouru",
-    contact: "+254 700 000000",
-    services: "Saturday 9am to 1pm ",
-    img: oriang
+    services: "Saturday 9am to 1pm",
+    img: oriang,
+    pastors: [
+      { name: "Pastor John Ouru", contact: "+254 768 553566" }
+    ]
   },
   {
     id: 9,
     name: "Dago Branch",
     location: "Homabay",
-    pastor: "Pastor Lilian",
-    contact: "+254 700 000000",
-    services: "Saturday 9am to 1pm ",
-    img: br7
+    services: "Saturday 9am to 1pm",
+    img: br7,
+    pastors: [
+      { name: "Pastor Nicholas Onyango", contact: "+254 742 954918" }
+    ]
   },
   {
     id: 10,
     name: "Kanyipir",
     location: "Kanyipir, Homabay",
-    pastor: "Pastor Mathews Bollo",
-    contact: "+254 795 297491",
-    services: "Saturday 9am to 1pm ",
-    img: kanyipir
+    services: "Saturday 9am to 1pm",
+    img: kanyipir,
+    pastors: [
+      { name: "Pastor Mathews Bollo", contact: "+254 795 297491" }
+    ]
   },
   {
     id: 11,
     name: "Got0yolo",
     location: "LuckySummer, Nairobi",
-    pastor: "Pastor Samuel Muga",
-    contact: "+254 728 092235",
-    services: "Saturday 9am to 1pm ",
-    img: br8
+    services: "Saturday 9am to 1pm",
+    img: br8,
+    pastors: [
+      { name: "Pastor Samuel Muga", contact: "+254 728 092235" }
+    ]
   },
   {
     id: 12,
-    name: "Rawinji Branch ",
+    name: "Rawinji Branch",
     location: "Oyugis, KenduBay",
-    pastor: "Pastor Evaline",
-    contact: "+254 728 092235",
-    services: "Saturday 9am to 1pm ",
-    img: br9
+    services: "Saturday 9am to 1pm",
+    img: br9,
+    pastors: [
+      { name: "Pastor Evaline", contact: "+254 728 092235" }
+    ]
   },
   {
     id: 13,
     name: "Got Kamajiwa",
     location: "Homabay",
-    pastor: "Pastor Lilian",
-    contact: "+254 700 000000",
-    services: "Saturday 9am to 1pm ",
-    img: br10
-  },
+    services: "Saturday 9am to 1pm",
+    img: br10,
+    pastors: [
+      { name: "Pastor John Onyango", contact: "+254 768 553566" },
+      { name: "Evangelist Corazon Alando", contact: "+254 719 319676" }
+
+    ]
+  }
 ];
