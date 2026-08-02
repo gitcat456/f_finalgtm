60f8102100435428826d840d251016a307637637
gitcat456 <okothdenzel65@gmail.com>
2026-08-02 11:52:01 +0300

    import HomePageSkeleton component for loading states

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import LoadingSpinner from './components/LoadingSpinner.jsx'
+import { HomePageSkeleton } from './components/skeletons/PageSkeletons.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Suspense fallback={<LoadingSpinner />}>
+    <Suspense fallback={<HomePageSkeleton />}>
       <App />
     </Suspense>
   </StrictMode>,
