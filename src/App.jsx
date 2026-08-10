4240bb8324dc4905d22b1193364f1c7aad6c379a
gitcat456 <okothdenzel65@gmail.com>
2026-08-10 23:19:31 +0300

    add AnalyticsTracker to app root for visitor tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRouter';
+import AnalyticsTracker from './components/AnalyticsTracker';
 
 const App = () => {
   return (
     <BrowserRouter>
+      <AnalyticsTracker />
       <AppRoutes />
     </BrowserRouter>
   );
