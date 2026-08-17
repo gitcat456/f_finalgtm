8b6dfd0bf22df14bce98b9a38db3f471f19aeb2f
gitcat456 <okothdenzel65@gmail.com>
2026-08-17 22:27:23 +0300

    add Founders and Clergy to sidebar navigation with HistoryEduIcon

--- src/admin/components/Sidebar.jsx
+++ src/admin/components/Sidebar.jsx
@@ -2,6 +2,7 @@ import { NavLink, useLocation } from 'react-router-dom';
 import DashboardIcon from '@mui/icons-material/DashboardOutlined';
 import LocationCityIcon from '@mui/icons-material/LocationCityOutlined';
 import EventIcon from '@mui/icons-material/EventOutlined';
+import HistoryEduIcon from '@mui/icons-material/HistoryEduOutlined';
 import PeopleIcon from '@mui/icons-material/PeopleOutlined';
 import AssessmentIcon from '@mui/icons-material/AssessmentOutlined';
 import SettingsIcon from '@mui/icons-material/SettingsOutlined';
@@ -19,6 +20,7 @@ const navSections = [
       { label: 'Visitors', path: '/admin/visitors', icon: AssessmentIcon },
       { label: 'Branches', path: '/admin/branches', icon: LocationCityIcon },
       { label: 'Events', path: '/admin/events', icon: EventIcon },
+      { label: 'Founders', path: '/admin/founders', icon: HistoryEduIcon },
       { label: 'Clergy', path: '/admin/clergy', icon: PeopleIcon },
     ],
   },
