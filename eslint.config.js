31f8dfc55646d22663a7ac49d0d69611af16df6c
gitcat456 <okothdenzel65@gmail.com>
2026-08-20 21:58:26 +0300

    add ESLint config for backend files with Node globals

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import { defineConfig, globalIgnores } from 'eslint/config'
 export default defineConfig([
   globalIgnores(['dist', 'node_modules', 'build']),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['src/**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
       reactHooks.configs['recommended-latest'],
@@ -33,4 +33,21 @@ export default defineConfig([
       'eqeqeq': ['warn', 'always'],
     },
   },
+  {
+    files: ['backend/**/*.{js,jsx}'],
+    extends: [js.configs.recommended],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: { ...globals.node, ...globals.express },
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]', argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+    },
+  },
 ])
+
